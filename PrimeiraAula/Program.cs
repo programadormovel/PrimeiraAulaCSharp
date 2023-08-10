c45af95177ed76e80aa7ecceedd79f458a37859b
programadormovel <programadormovel@gmail.com>
2023-08-09 22:36:37 -0300

    Add project files.

---
+++ PrimeiraAula/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiraAula
+{
+    internal class Program
+    {
+        // Atributos
+        // Métodos
+        static void Main(string[] args)
+        {
+            // algoritmo
+            Console.Write("Hello World!");
+            Console.WriteLine("Mais uma linha!");
+
+            string nome = "Maria José";
+
+            Console.WriteLine("O nome da pessoa: \n" + nome);
+
+            Console.Write("Entre com um novo nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("O nome da pessoa: " + nome);
+
+
+            Console.Write("Entre com um número: ");
+            int numero = Convert.ToInt16(Console.ReadLine());
+            Console.Write("O número digitado foi " + numero);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
